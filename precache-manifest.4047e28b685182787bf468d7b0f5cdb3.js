9891da3e13d0ed63c55a5c95e384e8d0f4799466
karin <karin.lien.c.l@chowsangsang.com>
2019-09-27 10:35:20 +0800

    Updates

---
+++ precache-manifest.4047e28b685182787bf468d7b0f5cdb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca066e457e8ae939e83e6070641d9288",
+    "url": "/proj/index.html"
+  },
+  {
+    "revision": "f442d180b43491f360ae",
+    "url": "/proj/static/css/main.fce2a8e7.chunk.css"
+  },
+  {
+    "revision": "1c37d8acb6a09b18d929",
+    "url": "/proj/static/js/2.7ae1eb90.chunk.js"
+  },
+  {
+    "revision": "f442d180b43491f360ae",
+    "url": "/proj/static/js/main.0a86cb11.chunk.js"
+  },
+  {
+    "revision": "11698ca9391bd8eb0528",
+    "url": "/proj/static/js/runtime-main.4db8b525.js"
+  },
+  {
+    "revision": "6042dfe885dc16b8e3b7fa20e9f831d3",
+    "url": "/proj/static/media/image.6042dfe8.png"
+  }
+]);
