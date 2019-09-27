18a67613889ed0e12e2196111b1581850b228174
karin <karin.lien.c.l@chowsangsang.com>
2019-09-27 16:47:13 +0800

    fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,9 @@
-import React, { createClass } from "react";
+import React from "react";
 
 import Layout from "../../components/Layout";
 import "./style.css";
 import PeopleIntro from "../../components/PeopleIntro";
 /*----------- */
-
-// import Ryan from "../../assets/home/people/Ryan.png";
-// import Apeach from "../../assets/home/people/Apeach.png";
-// import Tube from "../../assets/home/people/Tube.png";
-// import Muzi from "../../assets/home/people/Muzi.png";
-// import Frodo from "../../assets/home/people/Frodo.png";
-// import Neo from "../../assets/home/people/Neo.png";
-// import Con from "../../assets/home/people/C.png";
-// import Jay from "../../assets/home/people/Jay-G.png";
-
 class Home extends React.Component {
   state = { index: 0 };
   handleShow(i) {
