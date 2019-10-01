fb16c2df728eab8a0b119d9e24ceb807092179ef
karin <karin.lien.c.l@chowsangsang.com>
2019-10-01 14:20:05 +0800

    add product page

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -2,12 +2,60 @@ import React from "react";
 
 /*------------- */
 import Layout from "../../components/Layout";
+import "./style.css";
+import A from "../../assets/pro/ryan-small.jpg";
 
 class Product extends React.Component {
+  state = {
+    pro: []
+  };
+  async componentDidMount() {
+    const port = "2999";
+    const response = await fetch(`http://127.0.0.1:${port}/api/product/`);
+    const data = await response.json();
+    this.setState({
+      pro: data
+    });
+  }
   render() {
+    const { pro } = this.state;
     return (
       <Layout>
-        <div>Product</div>
+        <div className="pdt-big-box">
+          {pro.length !== 0 ? (
+            pro.map(item => (
+              <div className="pdt-box" key={item.name}>
+                <div className="pdt-img-box">
+                  <img
+                    src={require(`../../assets/pro/${item.productImg}`)}
+                    alt={item.name}
+                  />
+                </div>
+                <div className="pdt-content">
+                  <p>{item.name}</p>
+                  <p>
+                    {item.pdtSpecialPrice === item.pdtPrice ? (
+                      <span className="style-nospecialprice">
+                        {item.pdtPrice}
+                      </span>
+                    ) : (
+                      <>
+                        <span className="margin-5 special-price-style ">
+                          {item.pdtSpecialPrice}
+                        </span>
+                        <span className="special-price-true margin-5 price-style">
+                          {item.pdtPrice}
+                        </span>
+                      </>
+                    )}
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <span>no product</span>
+          )}
+        </div>
       </Layout>
     );
   }
