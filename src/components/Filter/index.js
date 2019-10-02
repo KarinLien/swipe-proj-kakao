392d57dffc43ad782d16ea494d9fa2fad5e1172f
karin <karin.lien.c.l@chowsangsang.com>
2019-10-02 14:33:26 +0800

    update filter content

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -4,11 +4,62 @@ import React from "react";
 import "./style.css";
 import down from "../../assets/common/dropdown_down.png";
 import up from "../../assets/common/dropdown_up.png";
+import selectTrue from "../../assets/common/category_icn-done.png";
+/**------------- */
+
+const selectData = [
+  {
+    name: "전체",
+    engName: "All",
+    img: "category_group_off.png",
+    imgName: "group"
+  },
+  {
+    name: "라이언",
+    engName: "Ryan",
+    img: "category_rion_off.png",
+    imgName: "rion"
+  },
+  {
+    name: "어피치",
+    engName: "Apeach",
+    img: "category_apeach_off.png",
+    imgName: "apeach"
+  },
+  {
+    name: "무지",
+    engName: "Muzi",
+    img: "category_muzi_off.png",
+    imgName: "muzi"
+  },
+  {
+    name: "프로도",
+    engName: "Frodo",
+    img: "category_frodo_off.png",
+    imgName: "frodo"
+  },
+  { name: "네오", engName: "Neo", img: "category_neo_off.png", imgName: "neo" },
+  {
+    name: "튜브",
+    engName: "Tube",
+    img: "category_tube_off.png",
+    imgName: "tube"
+  },
+  {
+    name: "제이지",
+    engName: "Jay-G",
+    img: "category_jayz_off.png",
+    imgName: "jayz"
+  },
+  { name: "콘", engName: "Con", img: "category_con_off.png", imgName: "con" }
+];
 
 class Filter extends React.Component {
   state = {
     AllCharacters: false,
-    Newarrivals: false
+    Newarrivals: false,
+    Characters: 0,
+    hoverNumber: 0
   };
   clickAllCharacters = () => {
     this.setState({
@@ -22,8 +73,18 @@ class Filter extends React.Component {
       Newarrivals: !this.state.Newarrivals
     });
   };
+  chooseCharacters = index => {
+    this.setState({
+      Characters: index
+    });
+  };
+  mouseEvent = index => {
+    this.setState({
+      hoverNumber: index
+    });
+  };
   render() {
-    const { Newarrivals, AllCharacters } = this.state;
+    const { Newarrivals, AllCharacters, Characters, hoverNumber } = this.state;
     return (
       <div className="filter-big-box">
         <div
@@ -42,7 +103,40 @@ class Filter extends React.Component {
         </div>
         {AllCharacters ? (
           <div className="filter-select-box-AllCharacters">
-            <div className="filter-select-cont-AllCharacters">BOX</div>
+            <div className="filter-select-cont-AllCharacters">
+              {selectData.map((item, index) => (
+                <div
+                  className="select-cont-box"
+                  key={index}
+                  onClick={() => this.chooseCharacters(index)}
+                  onMouseOver={() => this.mouseEvent(index)}
+                  onMouseLeave={() => this.mouseEvent(index)}
+                >
+                  <div>
+                    <span className="select-cont-box-span">
+                      <img
+                        src={require(`../../assets/filter/category_${
+                          item.imgName
+                        }_${
+                          Characters === index || hoverNumber === index
+                            ? "on"
+                            : "off"
+                        }.png`)}
+                        alt="All"
+                      />
+                    </span>
+                    {Characters === index ? (
+                      <span className="select-true">
+                        <img src={selectTrue} alt="select-true" />
+                      </span>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <span>{item.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
           ""
@@ -50,9 +144,9 @@ class Filter extends React.Component {
         {Newarrivals ? (
           <div className="filter-select-box-Newarrivals">
             <ul className="filter-select-cont-Newarrivals">
-              <li>BOX1</li>
-              <li>BOX2</li>
-              <li>BOX3</li>
+              <li>New arrivals</li>
+              <li>Price low to high</li>
+              <li>Price high to low</li>
             </ul>
           </div>
         ) : (
