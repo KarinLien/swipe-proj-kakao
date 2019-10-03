6a51430d24c8a6d0817097a7bd87e1a940402b91
karin <karin.lien.c.l@chowsangsang.com>
2019-10-03 17:24:52 +0800

    fix pdt index.js style.css

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -9,7 +9,6 @@ import { fetchProduct } from "../../store/actions";
 import { connect } from "react-redux";
 
 class Product extends React.Component {
-
   componentDidMount() {
     this.props.fetchProduct();
   }
@@ -20,40 +19,42 @@ class Product extends React.Component {
         <div className="float-right">
           <Filter />
         </div>
-        <div className="pdt-big-box">
-          {ProductData.length !== 0 ? (
-            ProductData.map(item => (
-              <div className="pdt-box" key={item.name}>
-                <div className="pdt-img-box">
-                  <img
-                    src={require(`../../assets/pro/${item.productImg}`)}
-                    alt={item.name}
-                  />
-                </div>
-                <div className="pdt-content">
-                  <p>{item.name}</p>
-                  <p>
-                    {item.pdtSpecialPrice === item.pdtPrice ? (
-                      <span className="style-nospecialprice">
-                        {item.pdtPrice}
-                      </span>
-                    ) : (
-                      <>
-                        <span className="margin-5 special-price-style ">
-                          {item.pdtSpecialPrice}
-                        </span>
-                        <span className="special-price-true margin-5 price-style">
+        <div className="pdt-big-box-padding-8">
+          <div className="pdt-big-box">
+            {ProductData.length !== 0 ? (
+              ProductData.map(item => (
+                <div className="pdt-box" key={item.name}>
+                  <div className="pdt-img-box">
+                    <img
+                      src={require(`../../assets/pro/${item.productImg}`)}
+                      alt={item.name}
+                    />
+                  </div>
+                  <div className="pdt-content">
+                    <p>{item.name}</p>
+                    <p>
+                      {item.pdtSpecialPrice === item.pdtPrice ? (
+                        <span className="style-nospecialprice">
                           {item.pdtPrice}
                         </span>
-                      </>
-                    )}
-                  </p>
+                      ) : (
+                        <>
+                          <span className="margin-5 special-price-style ">
+                            {item.pdtSpecialPrice}
+                          </span>
+                          <span className="special-price-true margin-5 price-style">
+                            {item.pdtPrice}
+                          </span>
+                        </>
+                      )}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <div>no product</div>
-          )}
+              ))
+            ) : (
+              <div>no product</div>
+            )}
+          </div>
         </div>
       </Layout>
     );
